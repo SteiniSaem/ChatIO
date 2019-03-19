cf6b2f086d23ffbeb79f25d68bd50d6193b6c852
SteiniSaem <thorsteinns16@ru.is>
2019-03-19 17:24:15 +0000

    navbar improved

--- ChatIO-client/src/components/Navbar/Navbar.js
+++ ChatIO-client/src/components/Navbar/Navbar.js
@@ -3,7 +3,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="../../../static/logo.png" className="logo" />
+      <div className="navbar-left">
+        <img src="../../../static/logo.png" className="logo" />
+        <span className="lambo-edition">ChatIO (Lamborghini edition)</span>
+      </div>
       <span>Siggi sux dix</span>
     </nav>
   );
