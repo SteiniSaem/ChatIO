fc7a2126815cdd0cbbbeb4b9316aa875b45a05e9
SteiniSaem <thorsteinns16@ru.is>
2019-03-19 17:17:28 +0000

    navbar added

---
+++ ChatIO-client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src="../../../static/logo.png" className="logo" />
+      <span>Siggi sux dix</span>
+    </nav>
+  );
+};
+
+export default Navbar;
