ed42649fac596dfd8735c5fb6e76383c1d9640e0
SteiniSaem <thorsteinns16@ru.is>
2019-03-21 14:51:31 +0000

    styling on chat fixed

--- ChatIO-client/src/components/ChatWindow/ChatWindow.js
+++ ChatIO-client/src/components/ChatWindow/ChatWindow.js
@@ -4,17 +4,22 @@ import Users from "../Users/Users";
 
 const ChatWindow = ({ room }) => {
   return (
-    <div className="chat-text">
-      <div className="chat-topic">
-        <h1>{room.topic}</h1>
+    <div className="not-room-list">
+      <div className="chat-text">
+        <div>
+          <div className="chat-topic">
+            <h1>{room.topic}</h1>
+          </div>
+          <div className="chat-box">
+            <p>chat text here</p>
+          </div>
+        </div>
+        <div className="text-box">
+          <input type="text" className="msg-input" />
+          <button className="send-btn sign-in-btn">Send</button>
+        </div>
       </div>
-      <div className="chat-box">
-        <p>chat text here</p>
-      </div>
-      <div className="text-box">
-        <input type="text" className="msg-input" />
-        <button className="send-btn sign-in-btn">Send</button>
-      </div>
+      <Users />
     </div>
   );
 };
