b05119606ae581f42066368b1fa21241c9221f55
SteiniSaem <thorsteinns16@ru.is>
2019-03-21 17:42:07 +0000

    more message stuff

--- ChatIO-client/src/components/ListViewItem/MsgListViewItem.js
+++ ChatIO-client/src/components/ListViewItem/MsgListViewItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const MsgListViewItem = props => {
+  console.log("MsgListViewItem shalllooooommmm!!!!");
+  console.log(props.listItem);
   return (
-    <li class="user-list">
-      <p className="msg-timestamp">{props.item.timestamp}</p>
-      <span className="msg-nick">{props.item.nick}: </span>>
-      <span>{props.item.message}</span>
+    <li className="msg">
+      <div className="msg-time">
+        {props.listItem.timestamp.getHours().toString()}:
+        {props.listItem.timestamp.getMinutes().toString()}{" "}
+        {props.listItem.timestamp.getDay().toString()}.
+        {props.listItem.timestamp.getMonth().toString()}.
+        {props.listItem.timestamp.getFullYear().toString()}
+      </div>
+      <span className="msg-nick">{props.listItem.nick}: </span>
+      <span>{props.listItem.message}</span>
     </li>
   );
 };
--- ChatIO-client/src/components/Messages/Messages.js
+++ ChatIO-client/src/components/Messages/Messages.js
@@ -2,9 +2,16 @@ import React from "react";
 import MsgListView from "../ListView/MsgListView";
 
 const Messages = ({ messages }) => {
+  const msg = [
+    {
+      nick: "Dixie Normus",
+      timestamp: new Date(),
+      message: "heyyy, this is a message"
+    }
+  ];
   return (
     <div>
-      <MsgListView messages={messages} />
+      <MsgListView messages={msg} />
     </div>
   );
 };
