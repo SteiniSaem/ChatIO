1608eaa987476d6ebab4790635c8651910fbf49f
SteiniSaem <thorsteinns16@ru.is>
2019-03-23 13:09:27 +0000

    user list updates when user leaves room

--- ChatIO-client/src/constants/index.js
+++ ChatIO-client/src/constants/index.js
@@ -3,6 +3,7 @@
 export const ADD_USER = "ADD_USER";
 export const UPDATE_ROOMS = "UPDATE_ROOMS";
 export const SET_CURRENT_ROOM = "SET_CURRENT_ROOM";
+export const PART_ROOM = "PART_ROOM";
 export const UPDATE_USERS = "UPDATE_USERS";
 export const UPDATE_TOPIC = "UPDATE_TOPIC";
 export const SERVER_MESSAGE = "SERVER_MESSAGE";
