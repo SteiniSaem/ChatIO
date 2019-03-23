7b6f72362859051e654ffb33bb315e2fc601404b
SteiniSaem <thorsteinns16@ru.is>
2019-03-23 15:18:09 +0000

    stuf

--- ChatIO-client/src/components/ListViewItem/UserListViewItem.js
+++ ChatIO-client/src/components/ListViewItem/UserListViewItem.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const UserListViewItem = ({ nickName }) => {
-  return (
-    <li className="user-list-item">
-      <span>{nickName}</span>
-    </li>
-  );
+class UserListViewItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { rooms, currentRoom, nickName } = this.props;
+    if (rooms[currentRoom].ops[nickName] == undefined) {
+      return (
+        <li className="user-list-item">
+          <span>{nickName}</span>
+          <button className="action-btn">Message</button>
+        </li>
+      );
+    } else {
+      return (
+        <li className="user-list-item">
+          <span>{nickName}</span>
+          <button className="action-btn">Message</button>
+          <button className="action-btn">Kick</button>
+          <button className="action-btn">Ban</button>
+        </li>
+      );
+    }
+  }
+}
+
+const mapStateToProps = reduxStoreState => {
+  console.log(reduxStoreState);
+  return {
+    nickName: reduxStoreState.user.nickName,
+    currentRoom: reduxStoreState.room.currentRoom,
+    rooms: reduxStoreState.room.rooms
+  };
 };
 
-export default UserListViewItem;
+export default connect(mapStateToProps)(UserListViewItem);
